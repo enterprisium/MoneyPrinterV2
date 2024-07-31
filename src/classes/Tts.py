f07f78544399e9e9937bce45f9bcda0cc1ced4bb
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-07-31 05:37:03 -0700

    Update Tts.py

--- src/classes/Tts.py
+++ src/classes/Tts.py
@@ -1,59 +1,25 @@
 import os
-
 from config import ROOT_DIR
-from TTS.utils.manage import ModelManager
-from TTS.utils.synthesizer import Synthesizer
+from TTS.api import TTS as XTTS_V2
 
 class TTS:
     """
-    Class for Text-to-Speech using Coqui TTS.
+    Class for Text-to-Speech using XTTS_V2.
     """
-    def __init__(self) -> None:
+    def __init__(self, language: str = "en", voice: str = "default") -> None:
         """
         Initializes the TTS class.
+
+        Args:
+            language (str): The language for TTS. Defaults to "en".
+            voice (str): The voice for TTS. Defaults to "default".
 
         Returns:
             None
         """
-        # Use os.path.join to construct the path
-        venv_site_packages = os.path.join("venv", "lib", "python3.10", "site-packages")
-
-        # Path to the .models.json file
-        models_json_path = os.path.join(
-            ROOT_DIR,
-            venv_site_packages,
-            "TTS",
-            ".models.json",
-        )
-
-        # Initialize the ModelManager
-        self._model_manager = ModelManager(models_json_path)
-
-        # Download tts_models/en/ljspeech/fast_pitch
-        self._model_path, self._config_path, self._model_item = \
-            self._model_manager.download_model("tts_models/en/ljspeech/tacotron2-DDC_ph")
-
-        # Download vocoder_models/en/ljspeech/hifigan_v2 as our vocoder
-        voc_path, voc_config_path, _ = self._model_manager. \
-            download_model("vocoder_models/en/ljspeech/univnet")
-        
-        # Initialize the Synthesizer
-        self._synthesizer = Synthesizer(
-            tts_checkpoint=self._model_path,
-            tts_config_path=self._config_path,
-            vocoder_checkpoint=voc_path,
-            vocoder_config=voc_config_path
-        )
-
-    @property
-    def synthesizer(self) -> Synthesizer:
-        """
-        Returns the synthesizer.
-
-        Returns:
-            Synthesizer: The synthesizer.
-        """
-        return self._synthesizer
+        self.language = language
+        self.voice = voice
+        self.synthesizer = XTTS_V2(language=self.language, voice=self.voice)
 
     def synthesize(self, text: str, output_file: str = os.path.join(ROOT_DIR, ".mp", "audio.wav")) -> str:
         """
